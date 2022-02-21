bbcb540313426798d38202927dd07e230641bd41
Darkbelg <stijn.sagaert@outlook.com>
2022-02-21 14:28:49 +0100

    Time to Refactor Episode 9

--- src/App.vue
+++ src/App.vue
@@ -3,225 +3,51 @@
     <div v-if="isOffline" class="absolute top-0 left-0 opacity-75 z-10 w-full text-center py-2 bg-red-300 border-b border-red-700 text-white">
       Sorry, it looks like you are offline
     </div>
-    <div class="flex flex-col flex-shrink-0 w-64 border-r border-gray-300 bg-gray-100">
-      <!-- Sidebar -->
-      <div class="h-0 overflow-auto flex-grow">
-        <div class="flex items-center h-8 px-3 group mt-4">
-          <div class="flex items-center flex-grow focus:outline-none">
-            <a href="#" class="ml-2 leading-none font-medium text-sm"
-              @click.prevent="showAllNotes"
-            >
-              All notes
-            </a>
-          </div>
-          <button class="add-note" @click="addNewNote">
-            <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
-            </svg>
-          </button>
-        </div>
-        <div class="mt-4">
-          <a href="#" class="flex items-center h-8 text-sm pl-8 pr-3"
-            v-for="note in notes"
-             @click.prevent = "openNote(note)"
-             :key="note.created"
-          >
-            <span class="ml-2 leading-none">
-              {{ new Date(note.created).toLocaleString() }}
-            </span>
-          </a>
-        </div>
-      </div>
-    </div>
-    <div class="flex flex-col flex-grow" v-if="Object.keys(activeNote).length">
-      <!-- Main Content - Editor -->
-      <div class="flex flex-col flex-grow overflow-auto">
-        <editor-content :editor="editor"></editor-content>
-      </div>
-      <div class="h-16 bg-gray-100 border-t border-gray-300 text-right">
-        <button @click="saveNote">Save Note</button>
-      </div>
-    </div>
-    <div class="flex flex-col flex-grow" v-else>
-      <!-- Main Content - Notes List -->
-      <div class="flex flex-col flex-grow overflow-auto">
-        <div v-for="note in notes" :key="note.created">
-          <div class="flex px-4 pt-3 pb-4">
-            <div class="prose my-2 mx-auto">
-              <div>
-                <span class="ml-1 text-xs text-gray-500">Created on {{ new Date(note.created).toLocaleString() }}</span>
-              </div>
-              <div v-html="note.content"></div>
-            </div>
-          </div>
-          <hr class="w-full">
-        </div>
-      </div>
-    </div>
+    <sidebar></sidebar>
+    <editor v-if="Object.keys(activeNote).length"></editor>
+    <notes v-else></notes>
   </div>
 </template>
 
 <script>
-import { Editor,EditorContent } from '@tiptap/vue-3';
-import StarterKit from '@tiptap/starter-kit';
+import Sidebar from "@/components/Sidebar";
+import Editor from "@/components/Editor";
+import Notes from "@/components/Notes";
+
 export default {
   name: 'App',
   components: {
-    EditorContent
-  },
-  data(){
-    return {
-      editor: null,
-      database: null,
-      notes: [],
-      activeNote: {},
-      isOffline: !navigator.onLine
-    }
-  },
-  async created(){
-    this.database = await this.getDatabase();
-    let notes = await this.getNotes();
-    this.notes = notes.reverse();
+    Sidebar,
+    Editor,
+    Notes
   },
   mounted(){
-    this.editor = new Editor({
-      content: '',
-      extensions: [
-        StarterKit
-      ],
-      editorProps: {
-        attributes: {
-          class: "prose my-6 mx-auto focus:outline-none"
-        }
-      }
-    });
     window.addEventListener('offline', () => {
-      this.isOffline = true;
+      this.$store.commit('updateIsOffline', true);
     });
     window.addEventListener('online', () => {
-      this.isOffline = false;
+      this.$store.commit('updateIsOffline', false);
 
       // sync up a user's data with an external api
       this.syncUserData();
     });
   },
   beforeUnmount() {
-    this.editor.destroy()
+    this.$store.state.dispatch('destroyEditor');
   },
   methods: {
-    async getDatabase(){
-      return new Promise((resolve,reject) => {
-        let db = window.indexedDB.open("notes") ;
-        // Do it to upgrade a excisting database
-        //let db = window.indexedDB.open("notes", 2) ;
-
-        db.onerror = e => {
-          reject('Error opening the database.')
-        };
-        //Is the same as:
-        //db.onerror = function (e) { }
-
-        db.onsuccess = e => {
-          console.log('db.onsuccess',e);
-          resolve(e.target.result);
-        };
-
-        db.onupgradeneeded = e => {
-          console.log('db.onupgradeneeded',e);
-          //only use when upgrading database. Can't delete what you don't have
-          // e.target.result.deleteObjectStore("notes");
-          e.target.result.createObjectStore("notes", { keyPath: "created" });
-        };
-      });
-    },
-    async saveNote(){
-      return new Promise((resolve, reject) => {
-        let noteStore =  this.database.transaction('notes','readwrite')
-            .objectStore('notes');
-        let noteRequest = noteStore.get(this.activeNote.created);
-
-        noteRequest.onerror = e => {
-          reject('Error saving the note in the database.');
-        }
-
-        noteRequest.onsuccess = e => {
-          let note = e.target.result;
-          note.content = this.editor.getHTML();
-
-          let updateRequest = noteStore.put(note);
-
-          updateRequest.onerror = e => {
-            reject ('Error storing the updated note in the database.');
-          };
-
-          updateRequest.onsuccess = e => {
-            let noteIndex = this.notes.findIndex(n => n.created === note.created);
-            this.notes[noteIndex] = note;
-            resolve();
-          };
-        }
-      });
-    },
-    async getNotes(){
-      return new Promise((resolve, reject) => {
-        // let transaction = this.database.transaction('notes');
-        // let notesStore = transaction.objectStore('notes');
-        // let notes = notesStore.getAll();
-        //
-        // notes.onsuccess = e => {
-        //   resolve(e.target.result);
-        // };
-
-        //Shorter chained alternative
-
-        let notes = this.database.transaction('notes')
-            .objectStore('notes')
-            .getAll();
-        notes.onsuccess = e => {
-            console.log('getNotes()', e.target.result);
-              resolve(e.target.result);
-            };
-        notes.onerror = e => {
-              console.log('failed getting the notes');
-              reject("no notes")
-        }
-      });
-    },
-    openNote(note){
-      this.editor.commands.setContent(note.content);
-      this.activeNote = note;
-    },
-    showAllNotes(){
-      //clear the editor
-      this.editor.commands.clearContent();
-      //set the activenote as an empty object
-      this.activeNote = {};
-    },
-    addNewNote(){
-      // add a blank note to the database
-      return new Promise((resolve, reject) => {
-        let transaction = this.database.transaction("notes","readwrite");
-        transaction.oncomplete = e => {
-          resolve();
-        }
-
-        let now = new Date();
-        let note = {
-          content: this.editor.getHTML(),
-          created: now.getTime()
-        };
-        // add that same note to the sidebar
-        this.notes.unshift(note);
-
-        // set the activeNote as the new note
-        this.activeNote = note;
-        transaction.objectStore("notes").add(note);
-      });
-    },
     syncUserData(){
       if (this.isOffline){
         return 'updating';
       }
+    }
+  },
+  computed: {
+    activeNote(){
+      return this.$store.state.activeNote;
+    },
+    isOffline(){
+      return this.$store.state.isOffline;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,143 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import './app.css'
 import './registerServiceWorker'
 
-createApp(App).mount('#app')
+import {createStore} from 'vuex'
+
+const store = createStore({
+    state() {
+        return {
+            editor: null,
+            database: null,
+            notes: [],
+            activeNote: {},
+            isOffline: !navigator.onLine
+        }
+    }
+    ,
+    mutations: {
+        updateEditor(state, editor) {
+            state.editor = editor;
+        },
+        updateDatabase(state, database) {
+            state.database = database;
+        },
+        updateNotes(state, notes) {
+            state.notes = notes;
+        },
+        updateActiveNote(state, note) {
+            state.activeNote = note
+        },
+        updateIsOffline(state, isOffline) {
+            state.isOffline = isOffline
+        }
+    }
+    ,
+    actions: {
+        init({dispatch}) {
+            dispatch('initDatabase').then(() => {
+                dispatch('initNotes');
+            }).catch((e) => {
+                console.error(e)
+            });
+        },
+        initDatabase({commit}) {
+            return new Promise((resolve, reject) => {
+                //initialize the database
+                let db = window.indexedDB.open("notes",2);
+                // Do it to upgrade a excisting database
+                //let db = window.indexedDB.open("notes", 2) ;
+
+                db.onerror = e => {
+                    console.error('Error opening the database');
+                };
+                //Is the same as:
+                //db.onerror = function (e) { }
+
+                db.onsuccess = e => {
+                    console.log('db.onsuccess', e);
+                    commit('updateDatabase', e.target.result);
+                };
+
+                db.onupgradeneeded = e => {
+                    console.log('db.onupgradeneeded', e);
+
+                    if (e.oldVersion === 1) {
+                        e.target.result.deleteObjectStore("notes");
+                    }
+                    //only use when upgrading database. Can't delete what you don't have
+                    // e.target.result.deleteObjectStore("notes");
+                    e.target.result.createObjectStore("notes", {keyPath: "created"});
+                };
+            })
+        },
+        initNotes({commit, state}) {
+            // initialize the notes array
+            let notes = state.database.transaction('notes')
+                .objectStore('notes')
+                .getAll();
+            notes.onsuccess = e => {
+                console.log('getNotes()', e.target.result);
+                commit('updateNotes', e.target.result);
+
+            };
+            notes.onerror = e => {
+                console.log('failed getting the notes');
+            }
+        },
+        saveNote({commit, state}) {
+            let noteStore = state.database.transaction('notes', 'readwrite')
+                .objectStore('notes');
+            let noteRequest = noteStore.get(state.activeNote.created);
+
+            noteRequest.onerror = e => {
+                console.error('Error saving the note in the database.');
+            }
+
+            noteRequest.onsuccess = e => {
+                let note = e.target.result;
+                note.content = state.editor.getHTML();
+
+                let updateRequest = noteStore.put(note);
+
+                updateRequest.onerror = e => {
+                    console.error('Error storing the updated note in the database.');
+                };
+
+                updateRequest.onsuccess = e => {
+                    let notes = state.notes;
+                    let noteIndex = notes.findIndex(n => n.created === note.created);
+                    notes[noteIndex] = note;
+                    commit('updateNotes', notes);
+                };
+            }
+        },
+        addNewNote({commit, state}) {
+            let transaction = state.database.transaction("notes", "readwrite");
+
+            let now = new Date();
+            let note = {
+                content: '',
+                created: now.getTime()
+            };
+            // add that same note to the sidebar
+            let notes = state.notes;
+            notes.unshift(note);
+            commit('updateNotes', notes);
+
+            // set the activeNote as the new note
+            commit('updateActiveNote', note);
+
+            transaction.objectStore("notes").add(note);
+        },
+        destroyEditor({commit, state}) {
+            state.editor.destroy();
+            commit('updateEditor', null);
+        }
+    }
+})
+
+createApp(App).use(store).mount('#app')
+
+store.dispatch('init')
